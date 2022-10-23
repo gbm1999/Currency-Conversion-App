ea37478acec0c76d95d63ea8b689988cb1b30a56
gbm1999 <gbm25@gcloud.ua.es>
2022-10-23 20:25:54 +0200

    Ejercicio 1 Caché sin funcionar

--- packages/backend/src/utils/fetch-rates.js
+++ packages/backend/src/utils/fetch-rates.js
@@ -1,12 +1,41 @@
 import undici from "undici";
 import { config } from "../config.js";
 
-export const fetchRates = async () => {
-  const endpoint = new URL(config.MONEYCONVERT_ENDPOINT);
-  // Prevent cached response.
-  endpoint.searchParams.set("_", Date.now());
+const endpoint = new URL(config.MONEYCONVERT_ENDPOINT);
+endpoint.searchParams.set("_", Date.now());
 
-  const { statusCode, body } = await undici.request(endpoint, {
+const cacheTime = 3600;
+var cache = {};
+let cacheTimer = 0;
+
+export const fetchRates = async () => {
+  // Prevent cached response.
+  const data = fetchWithCache("cache", cacheTime);
+  console.log(data);
+
+
+  return new Map([...Object.entries(data.rates)].sort());
+};
+
+const getCacheTimer  = time => {
+  const now = new Date().getTime();
+  if(cacheTimer < now + time) {
+    cacheTimer = now + time;
+  }
+  return cacheTimer;
+}
+
+const fetchWithCache = async (cacheName, time) => {
+  const now = new Date().getTime();
+  if(!cache[cacheName] || cache[cacheName].cacheTimer < now) {
+    cache[cacheName].data = await fetchURL();
+    cache[cacheName].cacheTimer = getCacheTimer(time);
+  }
+  return cache[cacheName].data;
+}
+
+const fetchURL = async () => {
+  const { statusCode, body } =  await undici.request(endpoint, {
     method: "GET",
   });
   if (statusCode < 200 || statusCode > 299) {
@@ -17,6 +46,5 @@ export const fetchRates = async () => {
   if (!data.rates || typeof data.rates !== "object") {
     throw new Error("Unexpected API response body");
   }
-
-  return new Map([...Object.entries(data.rates)].sort());
-};
+  return data;
+}
