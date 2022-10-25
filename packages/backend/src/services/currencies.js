1d0c86e8693402505b452c26d64d5eb27fe5b90a
gbm1999 <gbm25@gcloud.ua.es>
2022-10-25 17:23:03 +0200

    Ejercicio 2 currencies endpoint completado

--- packages/backend/src/services/currencies.js
+++ packages/backend/src/services/currencies.js
@@ -1,4 +1,4 @@
-//import { currencySymbols } from "../utils/currency-symbols.js";
+import { currencySymbols } from "../utils/currency-symbols.js";
 
 export class CurrenciesService {
   constructor(app) {
@@ -7,9 +7,18 @@ export class CurrenciesService {
 
   async get({ reply }) {
     try {
-      const currencies = {};
+      const currenciesRetrieved = currencySymbols;
 
-      reply.send({ currencies });
+      let currencies = {};
+      const code ={};
+      const code2 = {code:"", symbol:""};
+      for (const key in currenciesRetrieved) {
+        code2.code = key;
+        code2.symbol = currenciesRetrieved[key];
+        code[key] = code2;
+      }
+      currencies = code;
+      reply.send({ currencies  });
     } catch (error) {
       this.app.log.error(error);
       reply.internalServerError();
