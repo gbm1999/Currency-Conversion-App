52d0ca87fc9185537b44a18406cf925bf5ad5aef
gbm1999 <gbm25@gcloud.ua.es>
2022-10-26 15:48:22 +0200

    Ejercicio 2 Backend. Información dinámica obtenida desde la API

--- packages/backend/src/services/currencies.js
+++ packages/backend/src/services/currencies.js
@@ -1,4 +1,6 @@
 import { currencySymbols } from "../utils/currency-symbols.js";
+import { fetchRates } from "../utils/fetch-rates.js";
+
 
 export class CurrenciesService {
   constructor(app) {
@@ -11,8 +13,21 @@ export class CurrenciesService {
       let currencies = {};
       const code ={};
 
-      for (let key in currenciesRetrieved) {
-        code[key] = {code:key, symbol:currenciesRetrieved[key]};
+      //Obtenemos los rates de la API
+      const rates = await fetchRates();
+      //Filtramos los rates de currency-symbols con los rates de la API
+      //En asArray preparamos las claves de la api para hacer match 
+      const asArray = Array.from(rates.keys());
+      const filtered = Object.keys(currenciesRetrieved)
+      .filter(key => asArray.includes(key))
+      .reduce((obj, key) => {
+        obj[key] = currenciesRetrieved[key];
+        return obj;
+      }, {});
+
+      //Generamos el schema
+      for (let key in filtered) {
+        code[key] = {code:key, symbol:filtered[key]};
       }
       currencies = code;
       reply.send({ currencies  });
